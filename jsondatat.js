deace9199af8037baeb5059ee0c7b38c34ba3e02
luis.nascimento <luis.nascimento@maxifrota.com.br>
2020-01-08 17:41:18 -0300

    Alert e //chrome.browserAction.setBadgeBackgroundColor({ color: 'red' }); / chrome.browserAction.setBadgeText({text: val.event_desc});

--- jsondatat.js
+++ jsondatat.js
@@ -3,24 +3,24 @@ $(document).ready(function(){
 
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
         var yyyy = today.getFullYear();
     
         today = yyyy + '-' + mm + '-' + dd;    
         
-        alert(today);
+       // alert(today);
 			
 			  $.each(data, function(key, val){
 				if (val.event_begin == today ) {
-		//		  // var dataNiver = val.event_desc;
-                 alert("OK" + val.event_desc );
-                chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
-                chrome.browserAction.setBadgeText({text: val.event_desc});
+		       // var dataNiver = val.event_desc;
+                 alert("PARABÉNS " + val.event_desc );
+                //chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
+               // chrome.browserAction.setBadgeText({text: val.event_desc});
               }else {  
                 // alert(val.event_begin\n val.event_desc); 
-                 alert(val.event_begin + "\n" + val.event_desc);
-       //             //chrome.browserAction.setBadgeBackgroundColor({ color: 'blue' });
-                   //chrome.browserAction.setBadgeText({text: val.event_desc });
+                 //alert( val.event_desc + "\n" + val.event_begin);
+                  //chrome.browserAction.setBadgeBackgroundColor({ color: 'blue' });
+                   chrome.browserAction.setBadgeText({text: '' });
                }  
 				
                 
